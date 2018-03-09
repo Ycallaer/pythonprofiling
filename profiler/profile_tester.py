515620b3b0c72dd7e1a7b6e1aba06af81bf3e379
yvescallaert <yves.callaert@maersk.com>
2018-03-09 08:59:51 +0100

    filling the list in a more elegant way

--- profiler/profile_tester.py
+++ profiler/profile_tester.py
@@ -1,9 +1,7 @@
 import cProfile
 from profiler.examples.example_functions import increase_by_1_loop,increase_by_1_list_comp
 
-dummy_list=[]
-for i in range(0,10000):
-    dummy_list.append(i)
+dummy_list=list(range(0,10000))
 
 cProfile.run('increase_by_1_loop(dummy_list)', filename='first_iteration.cprof' )
 cProfile.run('increase_by_1_list_comp(dummy_list)', filename='second_iteration.cprof' )
